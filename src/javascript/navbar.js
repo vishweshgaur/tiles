a9af89812798d19cbd301338da00186a9a27f310
Basu Kumar <basutulsyan710@gmail.com>
2024-09-24 22:46:55 +0530

    index page is responsive

--- src/javascript/navbar.js
+++ src/javascript/navbar.js
@@ -9,14 +9,41 @@ function loadNavbar() {
 
     .then((data) => {
       console.log(nav);
-      nav[0].innerHTML = data;
+      for (let i = 0; i < nav.size(); i++) {
+        nav[i].innerHTML = data;
+      }
+
       // attachMenuListeners(); // Attach the menu functionality after loading
     });
 }
 
+const hamburger = document.getElementById("nav-btn");
+hamburger.addEventListener("click", () => {
+  const navbar = document.getElementById("navbar-links");
+  navbar.classList.replace("hidden", "visible");
+  hamburger.classList.replace("visible", "hidden");
+  cut.classList.replace("hidden", "visible");
+
+  const navCont = document.getElementById("nav-cont");
+  navCont.classList.replace("flex-row", "flex-col");
+  navCont.classList.replace("items-center", "items-start");
+});
+
+const cut = document.getElementById("cut");
+cut.addEventListener("click", () => {
+  const navbar = document.getElementById("navbar-links");
+  navbar.classList.replace("visible", "hidden");
+  cut.classList.replace("visible", "hidden");
+
+  const navCont = document.getElementById("nav-cont");
+  hamburger.classList.replace("hidden", "visible");
+  navCont.classList.replace("flex-col", "flex-row");
+  navCont.classList.replace("items-center", "items-start");
+  navCont.classList.remove("opacity-10");
+});
+
 // Function to handle menu toggle functionality
 // function attachMenuListeners() {
-//
 //   const mobileMenu = document.getElementById("mobile-menu");
 //   const menuIcon = document.getElementById("menu-icon");
 //   const closeIcon = document.getElementById("close-icon");
